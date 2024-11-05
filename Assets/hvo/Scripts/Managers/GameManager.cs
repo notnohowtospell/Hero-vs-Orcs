85aa855bc32ee9285ff0f6bbad8b3d3d7655b4e0
D.Huy <btt270798@gmail.com>
2024-11-05 09:19:29 +0000

    SingletonManager

--- Assets/hvo/Scripts/Managers/GameManager.cs
+++ Assets/hvo/Scripts/Managers/GameManager.cs
@@ -3,32 +3,8 @@
 
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+public class GameManager : SingletonManager<GameManager>
 {
-    protected virtual void Awake()
-    {
-        GameManager[] managers = FindObjectsByType<GameManager>(FindObjectsSortMode.None);
-        if (managers.Length > 1)
-        {
-            Destroy(gameObject);
-            return;
-        }
-    }
-
-    public static GameManager Get()
-    {
-        var tag = nameof(GameManager);
-        GameObject managerObject = GameObject.FindWithTag(tag);
-        if (managerObject != null)
-        {
-            return managerObject.GetComponent<GameManager>();
-        }
-
-        GameObject go = new(tag);
-        go.tag = tag;
-        return go.AddComponent<GameManager>();
-    }
-
     public void Test()
     {
         Debug.Log("Hello World!");
