187ebe651b070d02b81d50150f630544acfe156c
D.Huy <btt270798@gmail.com>
2024-11-06 07:54:05 +0000

    Get Input Position

--- Assets/hvo/Scripts/Managers/GameManager.cs
+++ Assets/hvo/Scripts/Managers/GameManager.cs
@@ -5,8 +5,21 @@ using UnityEngine;
 
 public class GameManager : SingletonManager<GameManager>
 {
-    public void Test()
+
+
+
+    void Update()
     {
-        Debug.Log("Hello World!");
+        Vector2 inputPosition = Input.touchCount > 0 ? Input.GetTouch(0).position : Input.mousePosition;
+
+        if (Input.GetMouseButtonUp(0) || (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended))
+        {
+            DeteckClick(inputPosition);
+        }
+    }
+
+    void DeteckClick(Vector2 inputPosition)
+    {
+        Debug.Log(inputPosition);
     }
 }
--- Assets/hvo/Scripts/Units/Unit.cs
+++ Assets/hvo/Scripts/Units/Unit.cs
@@ -14,8 +14,5 @@ public abstract class Unit : MonoBehaviour
         {
             m_Animator = animator;
         }
-
-        var manager = GameManager.Get();
-        manager.Test();
     }
 }
