09b82f2f2cdcb1d6658fa3fd25337128c5b4eb70
D.Huy <btt270798@gmail.com>
2024-11-06 08:00:58 +0000

    Prevent touch and mouse slides

--- Assets/hvo/Scripts/Managers/GameManager.cs
+++ Assets/hvo/Scripts/Managers/GameManager.cs
@@ -6,15 +6,23 @@ using UnityEngine;
 public class GameManager : SingletonManager<GameManager>
 {
 
-
+    private Vector2 m_InitialTouchPosition;
 
     void Update()
     {
         Vector2 inputPosition = Input.touchCount > 0 ? Input.GetTouch(0).position : Input.mousePosition;
 
+        if (Input.GetMouseButtonDown(0) || (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began))
+        {
+            m_InitialTouchPosition = inputPosition;
+        }
+
         if (Input.GetMouseButtonUp(0) || (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended))
         {
-            DeteckClick(inputPosition);
+            if (Vector2.Distance(m_InitialTouchPosition, inputPosition) < 10)
+            {
+                DeteckClick(inputPosition);
+            }
         }
     }
 
