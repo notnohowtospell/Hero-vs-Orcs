87af9d9397a403c2e689170eee80f5a63c36087e
D.Huy <btt270798@gmail.com>
2024-11-13 08:28:27 +0000

    Confirmation Bar

---
+++ Assets/hvo/Scripts/UI/ConfirmationBar.cs
@@ -0,0 +1,16 @@
+
+
+
+
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ConfirmationBar: MonoBehaviour
+{
+    [SerializeField] private Button m_ConfirmButton;
+    [SerializeField] private Button m_CancelButton;
+
+    public void Show(){}
+    public void Hide(){}
+}
