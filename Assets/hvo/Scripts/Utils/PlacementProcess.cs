fee3b6e8260977f206fd77e7c4f819720b2cf6a8
D.Huy <btt270798@gmail.com>
2024-11-13 08:13:52 +0000

    Check for blocked gameobjects

--- Assets/hvo/Scripts/Utils/PlacementProcess.cs
+++ Assets/hvo/Scripts/Utils/PlacementProcess.cs
@@ -104,14 +104,34 @@ public class PlacementProcess
 
     bool CanPlaceTile(Vector3Int tilePosition)
     {
-        return m_WalkableTilemap.HasTile(tilePosition) && !IsInUnreachableTilemap(tilePosition);
+        return
+            m_WalkableTilemap.HasTile(tilePosition) &&
+            !IsInUnreachableTilemap(tilePosition) &&
+            !IsBlockedByGameobject(tilePosition);
     }
 
     bool IsInUnreachableTilemap(Vector3Int tilePosition)
     {
-        foreach(var tilemap in m_UnreachableTilemaps)
+        foreach (var tilemap in m_UnreachableTilemaps)
         {
             if (tilemap.HasTile(tilePosition)) return true;
+        }
+
+        return false;
+    }
+
+    bool IsBlockedByGameobject(Vector3Int tilePosition)
+    {
+        Vector3 tileSize = m_WalkableTilemap.cellSize;
+        Collider2D[] colliders = Physics2D.OverlapBoxAll(tilePosition + tileSize / 2, tileSize * 0.5f, 0);
+
+        foreach (var collider in colliders)
+        {
+            var layer = collider.gameObject.layer;
+            if (layer == LayerMask.NameToLayer("Player"))
+            {
+                return true;
+            }
         }
 
         return false;
