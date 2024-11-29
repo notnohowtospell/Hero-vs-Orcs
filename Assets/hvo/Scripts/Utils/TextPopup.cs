b67384e05b82939f065cd07f9903ae2acd29eac0
D.Huy <btt270798@gmail.com>
2024-11-29 11:14:09 +0000

    Playing with anim curves

--- Assets/hvo/Scripts/Utils/TextPopup.cs
+++ Assets/hvo/Scripts/Utils/TextPopup.cs
@@ -36,7 +36,7 @@ public class TextPopup: MonoBehaviour
         m_Text.fontSize += m_FontSizeCurve.Evaluate(normalizedTime) / 5;
         m_Text.color = new Color(m_Text.color.r, m_Text.color.g, m_Text.color.b, alpha);
         float xOffset = m_XOffsetCurve.Evaluate(normalizedTime);
-        float yOffset = m_YOffsetCurve.Evaluate(normalizedTime);
+        float yOffset = m_YOffsetCurve.Evaluate(normalizedTime) * 1.5f;
 
         transform.position += new Vector3(xOffset, yOffset, 0) * Time.deltaTime;
     }
