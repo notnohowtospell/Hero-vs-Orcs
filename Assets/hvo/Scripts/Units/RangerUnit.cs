9af84e3b05b98555090e1977609d7f70e179e3b1
D.Huy <btt270798@gmail.com>
2024-12-04 12:07:54 +0000

    Shoot arrows

--- Assets/hvo/Scripts/Units/RangerUnit.cs
+++ Assets/hvo/Scripts/Units/RangerUnit.cs
@@ -1,6 +1,27 @@
 
+using System.Collections;
+using UnityEngine;
 
 public class RangerUnit: SoldierUnit
 {
+    [SerializeField] private Projectile m_ProjectilePrefab;
 
+    protected override void OnAttackReady(Unit target)
+    {
+        PerformAttackAnimation();
+        StartCoroutine(ShootProjectile(0.4f, target));
+    }
+
+    private IEnumerator ShootProjectile(float delay, Unit target)
+    {
+        yield return new WaitForSeconds(delay);
+
+        if (CurrentState == UnitState.Dead) yield return null;
+
+        if (target != null && target.CurrentState != UnitState.Dead)
+        {
+            var projectile = Instantiate(m_ProjectilePrefab, transform.position, Quaternion.identity);
+            projectile.Initialize(this, target, m_AutoAttackDamage);
+        }
+    }
 }
