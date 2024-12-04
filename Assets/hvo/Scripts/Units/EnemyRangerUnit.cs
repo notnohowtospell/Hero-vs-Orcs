8faff83e1502d9377c26d107b31e9e6b20a97344
D.Huy <btt270798@gmail.com>
2024-12-04 12:57:32 +0000

    Throw dynamites

--- Assets/hvo/Scripts/Units/EnemyRangerUnit.cs
+++ Assets/hvo/Scripts/Units/EnemyRangerUnit.cs
@@ -1,7 +1,33 @@
 
 
+using System.Collections;
+using UnityEngine;
 
 public class EnemyRangerUnit : EnemyUnit
 {
+    [SerializeField] private Projectile m_ProjectilePrefab;
 
+    protected override void OnAttackReady(Unit target)
+    {
+        PerformAttackAnimation();
+        StartCoroutine(ShootProjectile(0.4f, target));
+    }
+
+    protected override void PerformAttackAnimation()
+    {
+        m_Animator.SetTrigger("Attack");
+    }
+
+    private IEnumerator ShootProjectile(float delay, Unit target)
+    {
+        yield return new WaitForSeconds(delay);
+
+        if (CurrentState == UnitState.Dead) yield return null;
+
+        if (target != null && target.CurrentState != UnitState.Dead)
+        {
+            var projectile = Instantiate(m_ProjectilePrefab, transform.position, Quaternion.identity);
+            projectile.Initialize(this, target, m_AutoAttackDamage);
+        }
+    }
 }
